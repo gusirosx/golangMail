8e48dd680c1b5c707d9de918bbda60350f83513c
Gustavo Rodrigues <gsrodrigues280@gmail.com>
2022-07-04 22:12:32 -0300

    buildMessage

--- services/mail.go
+++ services/mail.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/smtp"
 	"os"
+	"strings"
 )
 
 // The smtp package implements the Simple Mail Transfer Protocol.
@@ -31,26 +32,11 @@ func ContactUs(mail types.Mail) error {
 		"gsrodrigues@example.com",
 	}
 
-	// Message.
-	message := []byte(
-		"Subject: " + mail.Subject + "\r\n" +
-			"From:" + from + "\r\n" +
-			"To:" + createStringOfSlice(to) + "\r\n" +
-			"Subject:" + mail.Subject + "\r\n" +
-			"\r\n" +
-			"Name: " + mail.FirstName + " " + mail.LastName + "\n" +
-			"Email: " + mail.Email + "\n" +
-			"Mobile: " + mail.Phone + "\n" +
-			"Subject: " + mail.Subject + "\n" +
-			"----------------------------------------------------------------" + "\n" +
-			mail.Body + "\n" + "\r\n",
-	)
-
 	// Authentication information for the sender email address.
 	auth := smtp.PlainAuth("", user, password, host)
 
 	// Sending email.
-	err := smtp.SendMail(host+port, auth, from, to, message)
+	err := smtp.SendMail(host+port, auth, from, to, []byte(buildMessage(mail, to)))
 	if err != nil {
 		log.Println(err.Error())
 		return err
@@ -60,10 +46,14 @@ func ContactUs(mail types.Mail) error {
 	return nil
 }
 
-func createStringOfSlice(emails []string) (toList string) {
-	for _, email := range emails {
-		str := fmt.Sprint(email + ", ")
-		toList = toList + str
-	}
-	return toList[0 : len(toList)-2]
+func buildMessage(mail types.Mail, toList []string) string {
+	//msg := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\r\n"
+	msg := fmt.Sprintf("From: %s\r\n", mail.Email)
+	msg += fmt.Sprintf("To: %s\r\n", strings.Join(toList, ";"))
+	msg += fmt.Sprintf("Subject: %s\r\n", mail.Subject)
+	msg += fmt.Sprintf("Name: %s %s\r\n", mail.FirstName, mail.LastName)
+	msg += fmt.Sprintf("Email: %s\r\n", mail.Email)
+	msg += fmt.Sprintf("Mobile: %s\r\n", mail.Phone)
+	msg += fmt.Sprintf("\r\n %s \r\n", mail.Body)
+	return msg
 }
